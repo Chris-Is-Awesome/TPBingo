bf7b5cd9b2be91dfd46c4203bcf49b5ac2f2e585
Chris is Awesome <chrisstarkofficial@gmail.com>
2020-08-25 21:40:53 -0700

    TPR: Add enemies, mini-bosses, and bosses

--- bingosync-app/generators/zelda_twilight_princess_sd_generator.js
+++ bingosync-app/generators/zelda_twilight_princess_sd_generator.js
@@ -66,6 +66,7 @@
 	var bingoList = {
 
 		// Item gets
+
 		FishingRodGet: {
 			name: "Get Fishing Rod",
 		},
@@ -472,19 +473,288 @@
 			name: "Get 3 Mirror Shards (from bosses)",
 		},
 
+		// Common Enemy Goals
+
+		AeralfosKill: {
+			name: "Kill an Aeralfos",
+		},
+		ArmosKill: {
+			name: "Kill an Armos",
+		},
+		ArmosClawshotKill: {
+			name: "Kill an Armos with the Clawshot",
+		},
+		BeamosKill: {
+			name: "Kill a Beamos",
+		},
+		BeamosBombKill: {
+			name: "Kill a Beamos with Bombs",
+		},
+		BeamosBallChainKill: {
+			name: "Kill a Beamos with the Ball & Chain",
+		},
+		BlueBokoblinKills: {
+			name: "Kill 5 Blue Bokoblins",
+		},
+		RedBokoblinKills: {
+			name: "Kill 5 Red Bokoblins",
+		},
+		ChuKills: {
+			name: "Kill 1 of Each Type of Chu (excl. Green Chu)",
+		},
+		FireBubbleKills: {
+			name: "Kill 5 Fire Bubbles",
+		},
+		IceBubbleKills: {
+			name: "Kill 5 Ice Bubbles",
+		},
+		ChillfosKills: {
+			name: "Kill 5 Chillfos",
+		},
+		FreezardKills: {
+			name: "Kill 3 Freezards (and their Mini-Freezards)",
+		},
+		DarknutKills: {
+			name: "Kill 4 Darknuts",
+		},
+		DekuLikeKills: {
+			name: "Kill 3 Deku Likes",
+		},
+		DodongoKills: {
+			name: "Kill 5 Dodongos",
+		},
+		DodongoKill: {
+			name: "Kill a Dodongo with a Bomb",
+		},
+		GuayKill: {
+			name: "Kill a Guay with the Slingshot",
+		},
+		AGPoeKill: {
+			name: "Kill an Arbiter's Grounds Poe",
+		},
+		AGPoeKill2: {
+			name: "Kill 2 Arbiter's Grounds Poes",
+		},
+		AGPoeKill3: {
+			name: "Kill 3 Arbiter's Grounds Poes",
+		},
+		AGPoeKill4: {
+			name: "Kill All 4 Arbiter's Grounds Poes",
+		},
+		PoeKills10: {
+			name: "Kill 10 Poes",
+		},
+		PoeKills20: {
+			name: "Kill 20 Poes",
+		},
+		PoeKills30: {
+			name: "Kill 30 Poes",
+			// TODO: Longer goal
+		},
+		PoeKills40: {
+			name: "Kill 40 Poes",
+			// TODO: Longer goal
+		},
+		PoeKills50: {
+			name: "Kill 50 Poes",
+			// TODO: Longer goal
+		},
+		PoeKills60: {
+			name: "Kill 60 Poes",
+			// TODO: Longer goal
+		},
+		LizalfosKills: {
+			name: "Kill 5 Lizalfos",
+		},
+		DynalfosKills: {
+			name: "Kill 2 Dynalfos",
+		},
+		ShellBladeKills: {
+			name: "Kill 4 Shell Blades",
+		},
+		ShellBladeWBKills: {
+			name: "Kill 2 Shell Blades with Water Bombs",
+		},
+		WolfosKills: {
+			name: "Kill 5 White Wolfos",
+		},
+		ZantHeadKills: {
+			name: "Kill 5 Zant Heads",
+		},
+		TorchSlugKills: {
+			name: "Kill 5 Torch Slugs as Wolf",
+		},
+		StalfosKills: {
+			name: "Kill 3 Stalfos with Ball & Chain",
+		},
+		TilewormKills: {
+			name: "Kill 3 Tileworms",
+		},
+		MoldormKills: {
+			name: "Kill 3 Moldorms",
+		},
+		GibdoKills: {
+			name: "Kill 5 Gibdos (Redeads)",
+		},
+		ToadpoliKill: {
+			name: "Kill a Toadpoli with Ball & Chain",
+		},
+		HelmasaurKills: {
+			name: "Kill 5 Helmasaur",
+		},
+		HelmasaurKill: {
+			name: "Kill a Helmasaur with its own Armor",
+		},
+		KeeseKill: {
+			name: "Kill a Keese with Ball & Chain",
+		},
+		BigBabaKill: {
+			name: "Kill a Big Baba",
+		},
+		BombskitKill: {
+			name: "Kill a Bombskit with a Bombling",
+		},
+		EndingBlowKills: {
+			name: "Kill 5 Enemies with Ending Blow",
+		},
+		BackSliceKills: {
+			name: "Kill 5 Enemies with Back Slice",
+		},
+		HelmSplitterKills: {
+			name: "Kill 5 Enemies with Helm Splitter",
+		},
+		MortalDrawKills: {
+			name: "Kill 10 Enemies with Mortal Draw",
+		},
+		JumpStrikeKills: {
+			name: "Kill 5 Enemies with Jump Strike",
+		},
+		GreatSpinKills: {
+			name: "Kill 5 Enemies with Great Spin",
+		},
+		IronBootsKills: {
+			name: "Kill 3 Enemies with Iron Boots",
+		},
+		SpinnerKills: {
+			name: "Kill 5 Enemies with Spinner",
+		},
+		BallAndChainKills: {
+			name: "Kill 10 Enemies with Ball & Chain",
+		},
+		ToTStatueKills: {
+			name: "Kill 3 Enemies with the Temple of Time Statue",
+		},
+		BombKills: {
+			name: "Kill 5 Enemies with Bombs",
+		},
+		SlingshotKills: {
+			name: "Kill 5 Enemies with Slingshot",
+		},
+		BowKills: {
+			name: "Kill 5 Enemies with Bow",
+		},
+		BoomerangStuns: {
+			name: "Stun 5 Enemies with Boomerang",
+		},
+		WolfKills: {
+			name: "Kill 10 Enemies as Wolf",
+		},
+		EnviromentKills: {
+			name: "Kill an Enemy with Pots/Barrels/Boxes/etc.",
+		},
+		FriendlyFire: {
+			name: "Make one Enemy Kill Another",
+		},
+
+		// Mini-Bosses
+
+		Ook: {
+			name: "Defeat Ook",
+		},
+		Daangoro: {
+			name: "Defeat Dangoro",
+		},
+		DekuToad: {
+			name: "Defeat Deku Toad",
+		},
+		DeathSword: {
+			name: "Defeat Death Sword",
+		},
+		DarkHammer: {
+			name: "Defeat DarkHammer",
+		},
+		ToTDarknut: {
+			name: "Defeat Temple of Time Darknut",
+		},
+		CityAeralfos: {
+			name: "Defeat City in the Sky Aeralfos",
+		},
+		PhantomZants: {
+			name: "Defeat both Phantom Zants",
+		},
+		SkullKid: {
+			name: "Defeat Skull Kid",
+		},
+		KingBulblin: {
+			name: "Defeat King Bulblin",
+		},
+
+		// Bosses
+
+		Diababa: {
+			name: "Defeat Diababa",
+		},
+		Fyrus: {
+			name: "Defeat Fyrus",
+		},
+		Morpheel: {
+			name: "Defeat Morpheel",
+		},
+		Stallord: {
+			name: "Defeat Stallord",
+		},
+		Blizzeta: {
+			name: "Defeat Blizzeta",
+		},
+		Armogohma: {
+			name: "Defeat Armogohma",
+		},
+		Argorok: {
+			name: "Defeat Argorok",
+		},
+		Zant: {
+			name: "Defeat Zant",
+		},
+
+		// Link
+
+
+
+		// World
+
+
+
+		// Checks
+
+
+
+		// Misc
+
+
+
 	};
 
 	var types = {
 
 		fishingRod: ["FishingRodGet", "EarringGet"], // TODO: Fishing goals
-		bow: ["BowGet", "Quiver1Get", "Quiver2Get"], // TODO: Armogohma, Fyrus
+		bow: ["BowGet", "Quiver1Get", "Quiver2Get"],
 		bomb: ["BombBagGet", "BombBag2Get", "BombBag3Get", "GiantBombBagGet"], // TODO: Bomb checks
-		claws: ["ClawGet", "DoubleClawGet"], // TODO: Argorok, claw checks (if any)
-		rod: ["DRodGet", "DRod2Get"], // TODO: Hurt enemy/self with statue, armogohma
+		claws: ["ClawGet", "DoubleClawGet"], // TODO: claw checks
+		rod: ["DRodGet", "DRod2Get"], // Sky char checks
 		bottle: ["BottleGet", "Bottle2Get", "Bottle3Get", "Bottle4Get"], // TODO: Bottle uses
 		questItem: ["MemoGet", "SketchGet", "LetterGet", "InvoiceGet", "StatueGet", "CharmGet", "HorseCallGet"],
 		skybook: ["SkyBookGet", "SkyCharGet", "SkyChar2Get", "SkyChar3Get", "SkyChar4Get", "SkyChar5Get", "SkyChar6Get"],
-		hiddenSkill: ["Skill1Get", "Skill2Get", "Skill3Get", "Skill4Get", "Skill5Get", "Skill6Get", "Skill7Get", "EndingBlowGet", "ShieldAttackGet", "BackSliceGet", "HelmSplitterGet", "MortalDrawGet", "JumpStrikeGet", "GreatSpinGet"], // TODO:  Kill enemy with x skill
+		hiddenSkill: ["Skill1Get", "Skill2Get", "Skill3Get", "Skill4Get", "Skill5Get", "Skill6Get", "Skill7Get", "EndingBlowGet", "ShieldAttackGet", "BackSliceGet", "HelmSplitterGet", "MortalDrawGet", "JumpStrikeGet", "GreatSpinGet"],
 		sword: ["WoodSwordGet", "OrdonSwordGet", "MasterSwordGet", "LightSwordGet"],
 		shield: ["WoodShieldGet", "OrdonShieldGet", "HylianShieldGet"], // TODO: Burn wooden shield
 		armor: ["ZoraArmorGet", "MagicArmorGet"], // TODO: Armor goals (eg. die wearing x armor)
@@ -492,17 +762,30 @@
 		bug: ["Bug4Get", "Bug8Get", "Bug12Get", "Bug16Get", "Bug20Get", "Bug24Get"], // TODO: Turn in bugs
 		poe: ["Poe10Get", "Poe20Get", "Poe30Get", "Poe40Get", "Poe50Get", "Poe60Get"], // TODO: Jovanni/poe checks
 		scent: ["YouthScentGet", "IliaScentGet", "PoeScentGet", "ReekfishScentGet", "MedicineScentGet"],
-		heart: ["Hearts5", "Hearts10", "Hearts15", "Hearts20", "HeartContainerGet", "HeartContainer2Get", "HeartContainer3Get", "HeartContainer4Get", "HeartContainer5Get", "HeartContainer6Get", "HeartContainer7Get", "HeartContainer8Get"], // TODO: Bosses
+		heart: ["Hearts5", "Hearts10", "Hearts15", "Hearts20", "HeartContainerGet", "HeartContainer2Get", "HeartContainer3Get", "HeartContainer4Get", "HeartContainer5Get", "HeartContainer6Get", "HeartContainer7Get", "HeartContainer8Get"],
 		rupee: ["ValidationRupee", "GreenRupee", "AllRupeeTypes", "OrangeRupees", "SilverRupees"],
 		forestTemple: ["FTItemsGet", "FTKeysGet", "FTOoccooGet", "DiababaKill"],
 		goronMines: ["GMItemsGet", "GMKeysGet", "GMOoccooGet", "FyrusKill"],
 		lakebedTemple: ["LBTItemsGet", "LBTKeysGet", "LBTOoccooGet", "MorpheelKill"],
-		arbitersGround: ["AGItemsGet", "AGKeysGet", "AGOoccooGet", "StallordKill"],
-		snowpeakRuins: ["SPRItemsGet", "SPRKeysGet", "SPROoccooGet", "SPRPumpkinGet", "SPRCheeseGet", "BlizzetaKill"],
-		templeOfTime: ["ToTItemsGet", "ToTKeysGet", "ToTOoccooGet", "ArmogohmaKill"],
-		cityInTheSky: ["CityItemsGet", "CityKeysGet", "CityOoccooGet", "ArgorokKill"],
+		arbitersGround: ["AGItemsGet", "AGKeysGet", "AGOoccooGet", "StallordKill"], // TO NOT DO: Poe checks
+		snowpeakRuins: ["SPRItemsGet", "SPRKeysGet", "SPROoccooGet", "SPRPumpkinGet", "SPRCheeseGet", "BlizzetaKill"], // TODO: Poe checks
+		templeOfTime: ["ToTItemsGet", "ToTKeysGet", "ToTOoccooGet", "ArmogohmaKill"], // TODO: Poe checks
+		cityInTheSky: ["CityItemsGet", "CityKeysGet", "CityOoccooGet", "ArgorokKill"], // TODO: Poe checks
 		palaceOfTwilight: ["PoTItemsGet", "PoTKeysGet", "ZantKill"],
 		hyruleCastle: ["HCItemsGet", "HCKeysGet"],
+		armosKills: ["ArmosKill", "ArmosClawshotKill"],
+		beamosKills: ["BeamosKill", "BeamosBombKill", "BeamosBallChainKill"],
+		bokoKills: ["RedBokoblinKills", "BlueBokoblinKills"],
+		chus: ["ChuKills"], // TODO: Chu jelly scoops
+		bubbleKills: ["FireBubbleKills", "IceBubbleKills"],
+		iceKills: ["ChillfosKills", "FreezardKills"],
+		dodongoKills: ["DodongoKill", "DodongoKills"],
+		agPoeKills: ["AGPoeKill", "AGPoeKill2", "AGPoeKill3", "AGPoeKill4", "AGPoes"], // Added "AGPoes" in case I forget to not include that as a goal
+		poeKills: ["PoeKills10", "PoeKills20", "PoeKills30", "PoeKills40", "PoeKills50", "PoeKills60"],
+		lizalfosKills: ["LizalfosKills", "DynalfosKills"],
+		helmaKills: ["HelmasaurKills", "HelmasaurKill"],
+		skillKills: ["EndingBlowKills", "BackSliceKills", "HelmSplitterKills", "MortalDrawKills", "JumpStrikeKills", "GreatSpinKills"],
+		shellBladeKills: ["ShellBladeKills", "ShellBladeWBKills"],
 
 	};
 
